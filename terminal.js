a87c4b83456521394ad5a9a6bf993528f95d6923
BIGRMAN28 <128552151+RyAnPr1Me@users.noreply.github.com>
2025-10-28 22:13:36 -0400

    Change shortcut from Ctrl+Shift+E to Ctrl+Shift+P

--- terminal.js
+++ terminal.js
@@ -10,7 +10,7 @@
 (function() {
     'use strict';
     document.addEventListener('keydown', function(e) {
-        if(e.ctrlKey && e.shiftKey && e.code==='KeyE'){
+        if(e.ctrlKey && e.shiftKey && e.code==='KeyP'){
             e.preventDefault();
             const terminalTab = window.open('', '_blank');
             if(!terminalTab) return;
